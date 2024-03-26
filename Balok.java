1afc1953f3e81df616f0256bf4c694965184661e
RioKelian1 <145636304+RioKelian1@users.noreply.github.com>
2024-03-26 11:45:57 +0700

    Add files via upload

---
+++ Balok.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Balok extends BangunRuang {
+    private double panjang;
+    private double lebar;
+    private double tinggi;
+
+    public Balok(String name) {
+        super(name); 
+    }
+    
+    @Override
+    public void inputNilai() {
+        super.inputNilai();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Input panjang: ");
+            panjang = scanner.nextDouble();
+            System.out.print("Input lebar: ");
+            lebar = scanner.nextDouble();
+            System.out.print("Input tinggi: ");
+            tinggi = scanner.nextDouble();
+        }
+    }
+
+    // Method untuk menghitung luas permukaan balok
+    @Override
+    public void luasPermukaan() {
+        double hasil = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi));
+        super.luasPermukaan();
+        System.out.println("Luas permukaan : " + hasil);
+    }
+
+    // Method untuk menghitung volume balok
+    @Override
+    public void volume() {
+        double hasil = panjang * lebar * tinggi;
+        super.volume();
+        System.out.println("Hasil Volume : " + hasil);
+    }
+}
